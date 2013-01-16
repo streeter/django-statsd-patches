06c8ceca39aa9743856c4da9d879a163f56cdce8
Chris Streeter <chris@chrisstreeter.com>
2013-01-16 09:16:34 -0800

    Improve the setup file.

--- setup.py
+++ setup.py
@@ -1,17 +1,40 @@
-from setuptools import setup
+#!/usr/bin/env python
 
+version = '0.0.2'
+
+import os
+import sys
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist upload')
+    sys.exit()
+
+
+packages = [
+    'django_statsd_patches',
+    'django_statsd_patches/patches'
+]
+
+requires = [
+    'django-statsd-mozilla',
+]
 
 setup(
     name='django-statsd-patches',
-    version='0.0.2',
+    version=version,
     description='Patch and time some additional django libraries',
     long_description=open('README.rst').read(),
     author='Chris Streeter',
     author_email='chris@chrisstreeter.com',
     license='BSD',
-    install_requires=['django-statsd-mozilla'],
-    packages=['django_statsd_patches',
-              'django_statsd_patches/patches'],
+    install_requires=requires,
+    packages=packages,
     url='https://github.com/streeter/django-statsd-patches',
     include_package_data=True,
     zip_safe=False,
