042e88c46a508ca11e9e3681fb818275d1a45379
Chris Streeter <chris@chrisstreeter.com>
2013-01-16 09:20:44 -0800

    Some more classifiers.

--- setup.py
+++ setup.py
@@ -48,5 +48,7 @@ setup(
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Software Development :: Libraries :: Python Modules',
         ],
     )
